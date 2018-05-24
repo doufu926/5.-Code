40a8a0392317c77ed94bb1ed684f28a8ba474175
doufu926 <tongchenchen@gmail.com>
2018-05-24 19:16:22 +0800

    new target customer approach

---
+++ data preparation_new.R
@@ -0,0 +1,90 @@
+rm(list = ls())
+# load library
+library(plyr)
+library(data.table)
+
+# get work directory
+wd=getwd();
+# load function
+source(file.path(wd,"functions","DateProcess.R",fsep="/"))
+source(file.path(wd,"functions","DateProcess2.R",fsep="/"))
+source(file.path(wd,"functions","TimeProcess.R",fsep="/"))
+# for active
+source(file.path(wd,"functions","TargetCustomers2.R",fsep="/"))
+source(file.path(wd,"functions","FeatureDemo.R",fsep="/"))
+source(file.path(wd,"functions","FeatureDrive.R",fsep="/"))
+source(file.path(wd,"functions","FeatureFrequency.R",fsep="/"))
+source(file.path(wd,"functions","FeatureRecency.R",fsep="/"))
+source(file.path(wd,"functions","FeatureMoney.R",fsep="/"))
+source(file.path(wd,"functions","FeatureDelay.R",fsep="/"))
+source(file.path(wd,"functions","Featureparts.R",fsep="/"))
+source(file.path(wd,"functions","FeatureDuration.R",fsep="/"))
+source(file.path(wd,"functions","FeatureComplain.R",fsep="/"))
+source(file.path(wd,"functions","FeatureComplain2.R",fsep="/"))
+
+#############################################################
+## set parameters
+dealer.name <- "15099"
+branch.name <- "HO"
+# for active customer
+cutoff <- as.Date("2017-04-01")
+
+#############################################################
+## load in data
+file.list <- list.files("../4. Data/raw data/GS", pattern = "GS")
+data.raw = do.call(rbind.fill, lapply(file.path("../4. Data/raw data/GS/",file.list,fsep = ""), function(x) fread(x, encoding = "UTF-8")))
+
+# Select dealer data
+data.raw <- setDT(data.raw)
+data.all <- data.raw[DLR_CD == dealer.name & BRC_CD == branch.name,]
+data.all <- as.data.frame(data.all)
+
+# remove duplicates
+data.all <- data.all[!duplicated(data.all),]
+
+# process service date
+data.all <- DateProcess(data.all,'JOB_ORD_DT')
+data.all <- DateProcess(data.all,'VEH_SOLD_DT')
+data.all <- DateProcess(data.all,'CUST_DOB')
+data.all <- DateProcess(data.all,'ACT_RPR_START_DT')
+data.all <- DateProcess(data.all,'ACT_RPR_END_DT')
+data.all <- TimeProcess(data.all,'RPR_START_TM')
+data.all <- TimeProcess(data.all,'RPR_END_TM')
+
+#############################################################
+## customer filtering
+output <- TargetCustomers2(data.all, dealer.name, branch.name,cutoff)
+# unique target customer list
+cust.target <- output$cust.target
+# raw data,unique service records, dealer, non-fleet, na.omit
+visits.all <- output$visits.all
+
+# # save target customers data
+# data.target <- subset(data.all,data.all$VIN_NO%in%cust.target$VIN_NO)
+# fwrite(data.target, paste("../4. Data/Processed data/",dealer.name,"_target customer data.csv",sep = ""),row.names = FALSE)
+
+#############################################################
+## feature construction
+# demo
+cust.target <- FeatureDemo(data.all,visits.all,cust.target,cutoff)
+
+# driving bahavior
+cust.target <- FeatureDrive(data.all,cust.target,cutoff)
+
+# frequency
+cust.target <- FeatureFrequency(visits.all,cust.target,cutoff)
+
+# recency & interval
+cust.target <- FeatureRecency(visits.all,cust.target,cutoff)
+
+# monetary
+cust.target <- FeatureMoney(data.all,cust.target,cutoff)
+
+# delay
+cust.target <- FeatureDelay(data.all,cust.target,cutoff)
+
+# parts
+cust.target <- Featureparts(data.all,cust.target,cutoff)
+
+# service time
+cust.target <- FeatureDuration(data.all,cust.target,cutoff)
--- functions/TargetCustomers2.R
+++ functions/TargetCustomers2.R
@@ -1,24 +1,39 @@
 # find target customers
 # non-fleet, service at dealer, regular customer, retained in last year
-TargetCustomers <- function(data.all,dealer.name,branch.name, cutoff){
+TargetCustomers2 <- function(data.all,dealer.name,branch.name, cutoff){
   # did service at targeted dealer & branch
   data.target <- subset(data.all, data.all$DLR_CD==dealer.name)
   data.target <- subset(data.target, data.target$BRC_CD==branch.name)
+  print(length(unique(data.target$VIN_NO)))
+  
+  # fitler customer serviced in last 12 months
+  data.target$retain.before <- data.target$JOB_ORD_DT> cutoff-365 & data.target$JOB_ORD_DT<= cutoff
+  temp <- aggregate(retain.before~VIN_NO+CUST_ID_NO, data = data.target, sum)
+  # define next year retention status
+  cust.target.unique <- unique(subset(temp,temp$retain.before>0))
+  cust.target.unique$key <- paste(cust.target.unique$VIN_NO,cust.target.unique$CUST_ID_NO)
+  data.target$key <- paste(data.target$VIN_NO,data.target$CUST_ID_NO)
+  data.target <- subset(data.target,data.target$key%in%cust.target.unique$key)
+  print(length(unique(data.target$VIN_NO)))
   
   # non-fleet customers
   data.target <- subset(data.target, data.target$CUST_ID_TYP=="C")
+  print(length(unique(data.target$VIN_NO)))
   
   # find customer max service interval
   data.target <- unique(data.target[,c('VIN_NO','JOB_ORD_DT','VEH_SOLD_DT')])  # find unique service visits
   data.target <- na.omit(data.target)  # remove records with missing information
   data.target <- data.target[order(data.target$VIN_NO,data.target$JOB_ORD_DT),]  # sort records by vin and service date
+  print(length(unique(data.target$VIN_NO)))
   
   # filter customers serviced before cut off date, and define regular customer
   cust.target <- subset(data.target, data.target$JOB_ORD_DT<= cutoff)
+  print(length(unique(cust.target$VIN_NO)))
   # calculate service interval since purchase
   cust.target$SRV_INT <- as.numeric(cust.target$JOB_ORD_DT-cust.target$VEH_SOLD_DT)
   # filter customers service time <20 days from purchase, presales customers or revisit customers
   cust.target <- subset(cust.target, cust.target$SRV_INT>20)
+  print(length(unique(cust.target$VIN_NO)))
   # calculate service interval between two service
   cust.target$SRV_INT2 <- ave(cust.target$SRV_INT, cust.target$VIN_NO, FUN = function(x) c(min(x), diff(x)))
   cust.target$SRV_INT3 <- ave(cust.target$SRV_INT, cust.target$VIN_NO, FUN = function(x) c(0, diff(x)))
@@ -29,27 +44,23 @@ TargetCustomers <- function(data.all,dealer.name,branch.name, cutoff){
   temp2$SRV_INT2 <- temp1$SRV_INT2
   temp2$SRV_INT_MAX <- apply(temp2[,c('SRV_INT3','SRV_INT2')],1,min)
   cust.target$SRV_INT_MAX <- temp2$SRV_INT_MAX[match(cust.target$VIN_NO,temp2$VIN_NO)]  # assign to each record
-  # filter customers max interval >15 month, non-loyal/regular customer until cut off date???
+  # # filter customers max interval >15 month, non-loyal/regular customer until cut off date???
   # cust.target <- subset(cust.target, cust.target$SRV_INT_MAX<=15*30)
   
   # get all service record for those customers including results
   cust.target <- subset(data.target, data.target$VIN_NO%in%cust.target$VIN_NO)
   # fitler customer serviced in last 12 months
-  # cust.target$retain.before2 <- cust.target$JOB_ORD_DT> cutoff-365 & cust.target$JOB_ORD_DT<= cutoff
-  cust.target$retain.before <- cust.target$JOB_ORD_DT> cutoff & cust.target$JOB_ORD_DT<= cutoff+365
-  cust.target$retain <- cust.target$JOB_ORD_DT> cutoff+365 & cust.target$JOB_ORD_DT<= cutoff+365*2
+  cust.target$retain.before <- cust.target$JOB_ORD_DT> cutoff-365 & cust.target$JOB_ORD_DT<= cutoff
+  cust.target$retain <- cust.target$JOB_ORD_DT> cutoff & cust.target$JOB_ORD_DT<= cutoff+365
   temp1 <- aggregate(retain.before~VIN_NO, data = cust.target, sum)
   temp2 <- aggregate(retain~VIN_NO, data = cust.target, sum)
-  # temp3 <- aggregate(retain.before2~VIN_NO, data = cust.target,sum)
   temp2$retain.before <- temp1$retain.before[match(temp2$VIN_NO,temp1$VIN_NO)]
-  # temp2$retain.before2 <- temp3$retain.before2[match(temp2$VIN_NO,temp3$VIN_NO)]
   # define next year retention status
-  # select customer inactive
-  cust.target.unique <- unique(subset(temp2,temp2$retain.before==0))
-  # customer retained until last year
-  # cust.target.unique <- subset(cust.target.unique,cust.target.unique$retain.before2>0)
+  cust.target.unique <- unique(subset(temp2,temp2$retain.before>0))
+  print(length(unique(cust.target.unique$VIN_NO)))
   cust.target.unique$retain[cust.target.unique$retain>0] <- 1
   cust.target.unique$retain.before=NULL
+  print(sum(cust.target.unique$retain))
   
   # output
   output <- list("cust.target" = cust.target.unique, "visits.all" = data.target)
